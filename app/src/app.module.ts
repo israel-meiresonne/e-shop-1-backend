a557b375d255c1a693b8f3e31f70ea9dc23aaf8e
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-01-26 08:38:26 +0000

    app: initialize basket routes

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductsModule } from './products/products.module';
+import { BasketModule } from './basket/basket.module';
 
 @Module({
-  imports: [UserModule, ProductsModule],
+  imports: [UserModule, ProductsModule, BasketModule],
   controllers: [AppController],
   providers: [AppService],
 })
