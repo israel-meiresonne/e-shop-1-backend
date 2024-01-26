7887e3f119d2c79ebd9f4927c1f9b22e23880dc1
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-01-26 17:23:15 +0000

    user: implement endpoints

--- app/src/user/user.controller.ts
+++ app/src/user/user.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Delete, Body } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto, DeleteUserDto, LoginUserDto } from './dto';
 
 @Controller('user')
 export class UserController {
@@ -12,23 +11,13 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Delete()
+  delete(@Body() deleteUserDto: DeleteUserDto) {
+    return this.userService.delete(deleteUserDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.userService.login(loginUserDto);
   }
 }
--- app/src/user/user.service.ts
+++ app/src/user/user.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto, DeleteUserDto, LoginUserDto } from './dto';
 
 @Injectable()
 export class UserService {
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return `This action adds a new user: ${JSON.stringify(createUserDto)}`;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  delete(deleteUserDto: DeleteUserDto) {
+    return `This action delete the user: ${JSON.stringify(deleteUserDto)}`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  login(loginUserDto: LoginUserDto) {
+    return `This action log in an user: ${JSON.stringify(loginUserDto)}`;
   }
 }
