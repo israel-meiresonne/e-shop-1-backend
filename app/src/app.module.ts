50287a2bb301d7157bcec2a4cda59ddca59312e8
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-01-26 08:37:08 +0000

    app: initialize products routes

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
