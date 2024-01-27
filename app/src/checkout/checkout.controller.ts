aec4f23318a6c8e14e89d999692b2fc2ce56ebb0
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-01-27 13:41:00 +0000

    checkout: implement endpoints

--- app/src/checkout/checkout.controller.ts
+++ app/src/checkout/checkout.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { CheckoutService } from './checkout.service';
-import { CreateCheckoutDto } from './dto/create-checkout.dto';
-import { UpdateCheckoutDto } from './dto/update-checkout.dto';
+import { CreateCheckoutDto } from './dto';
 
 @Controller('checkout')
 export class CheckoutController {
@@ -10,25 +9,5 @@ export class CheckoutController {
   @Post()
   create(@Body() createCheckoutDto: CreateCheckoutDto) {
     return this.checkoutService.create(createCheckoutDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.checkoutService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.checkoutService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCheckoutDto: UpdateCheckoutDto) {
-    return this.checkoutService.update(+id, updateCheckoutDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.checkoutService.remove(+id);
   }
 }
