04651a4467bf4f93b927ba69c9eaef6c2258606b
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-01-27 13:38:52 +0000

    checkout: add DTO

--- app/src/checkout/dto/create-checkout.dto.ts
+++ app/src/checkout/dto/create-checkout.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCheckoutDto {}
+import { IsInt, IsString } from 'class-validator';
+
+export class CreateCheckoutDto {
+  @IsString()
+  readonly firstname: string;
+
+  @IsString()
+  readonly lastname: string;
+
+  @IsInt()
+  readonly digit: string;
+}
