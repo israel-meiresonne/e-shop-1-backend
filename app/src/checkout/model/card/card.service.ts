08ec6e7c03d1f6440b10d4e0b368f713be5994ff
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-01-30 13:36:42 +0000

    checkout: in Card's service class, add `.toResponse()`

--- app/src/checkout/model/card/card.service.ts
+++ app/src/checkout/model/card/card.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCheckoutDto } from '../../dto';
+import { CardResponse, CardEntity } from '.';
 
 @Injectable()
 export class CardService {
   create(createCheckoutDto: CreateCheckoutDto) {
     return `This action adds a new checkout: ${JSON.stringify(createCheckoutDto)}`;
+  }
+
+  private toResponse(cardEntity: CardEntity): CardResponse {
+    return {
+      id: cardEntity.id,
+    };
   }
 }
