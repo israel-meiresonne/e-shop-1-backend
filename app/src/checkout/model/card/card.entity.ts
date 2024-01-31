999a9441d29d7b5f0cd70e808970287b732f3f94
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-01-31 14:57:50 +0000

    checkout: Create table `Card` in Database (MySQL)

--- app/src/checkout/model/card/card.entity.ts
+++ app/src/checkout/model/card/card.entity.ts
@@ -1,6 +1,19 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Card')
 export class CardEntity {
+  @PrimaryColumn({ type: 'varchar', length: 30 })
+  userId: string;
+
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'varchar', length: 50 })
   firstname: string;
+
+  @Column({ type: 'varchar', length: 50 })
   lastname: string;
+
+  @Column({ type: 'varchar', length: 32 })
   digit: string;
 }
--- app/src/shared/entities.ts
+++ app/src/shared/entities.ts
@@ -1,11 +1,13 @@
 import { BasketProductEntity } from '../basket/model/basket-product';
+import { CardEntity } from '../checkout/model/card';
 import { PictureEntity } from '../products/model/picture';
 import { ProductEntity } from '../products/model/product';
 import { UserEntity } from '../user/model/user';
 
 export const Entities = [
-  UserEntity,
-  ProductEntity,
-  PictureEntity,
   BasketProductEntity,
+  CardEntity,
+  PictureEntity,
+  ProductEntity,
+  UserEntity,
 ];
