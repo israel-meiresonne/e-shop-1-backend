7a6e8cea23999d506a2fdc54fdaad7b14069b938
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-01-31 12:38:04 +0000

    config: add config for `MySQL`

---
+++ app/src/config/databases.ts
@@ -0,0 +1,12 @@
+const MySQL = {
+  type: 'mysql',
+  host: '192.168.0.14',
+  port: 7306,
+  username: 'test',
+  password: 'test',
+  database: 'test',
+  entities: [],
+  synchronize: true,
+};
+
+export const DatabaseConf = MySQL;
