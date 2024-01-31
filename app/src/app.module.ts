999a6bf47b1020fa2eb39feca4b0a4b39c45c3c3
Israel Meiresonne <israelmeiresonne97@gmail.com>
2024-01-31 12:42:33 +0000

    app: connect to MySQL server

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -5,9 +5,23 @@ import { UserModule } from './user/user.module';
 import { ProductsModule } from './products/products.module';
 import { BasketModule } from './basket/basket.module';
 import { CheckoutModule } from './checkout/checkout.module';
+import { DatabaseConf } from './config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Entities } from './shared/entities';
+
+const filledDatabaseConf = {
+  ...DatabaseConf,
+  entities: Entities,
+} as TypeOrmModuleOptions;
 
 @Module({
-  imports: [UserModule, ProductsModule, BasketModule, CheckoutModule],
+  imports: [
+    TypeOrmModule.forRoot(filledDatabaseConf),
+    UserModule,
+    ProductsModule,
+    BasketModule,
+    CheckoutModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
